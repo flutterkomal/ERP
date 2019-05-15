8141b9320c13e82ca68fa47fa3b92ffd9cff7667
komal.dhavan <dhavankomal@gmail.com>
2019-05-15 11:47:10 +0530

    delete unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+
 import 'package:erp_flutter/screens/CompanyConfig.dart';
-import 'package:erp_flutter/screens/SplashScreen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,4 +17,59 @@ void main() {
      // '/dashboard': (context) => ThirdScreen(),
     },
   ));
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  /*startTime() async {
+    var _duration = new Duration(seconds: 2);
+    return new Timer(_duration,navigationPage);
+  }
+  void navigationPage() {
+    // ignore: argument_type_not_assignable
+
+        // ignore: argument_type_not_assignable
+       *//* () => Navigator.push(context,
+        MaterialPageRoute(builder: (context) => CompanyPage()));*//*
+        // ignore: argument_type_not_assignable
+        Navigator.of(context).pushReplacementNamed("/company");
+    //Navigator.of(builder: (context)).pushReplacementNamed("/company");
+  }*/
+  @override
+  void initState() {
+    super.initState();
+    new Future.delayed(
+        const Duration(seconds: 5),
+            () => Navigator.push(context,
+          MaterialPageRoute(builder: (context) => CompanyPage()),
+        ));
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /* appBar: AppBar(
+        title: Text('Splash Screen'),
+        backgroundColor: Colors.green,
+          centerTitle:true,
+      ),*/
+      body: Center(
+        child:Text(
+          // ignore: argument_type_not_assignable
+          "Flutter",
+          style: TextStyle(
+              color: Colors.green,
+              fontSize: 50,
+              fontStyle: FontStyle.italic
+          ),
+        ),
+      ),
+    );
+  }
+
 }
